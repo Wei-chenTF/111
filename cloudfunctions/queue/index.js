6958630a7b7b1c98d59a6a4d4d41ae443c918761
unknown <you@example.com1439178779@qq.com>
2020-11-12 22:07:13 +0800

    删除按钮，美化页面

--- cloudfunctions/queue/index.js
+++ cloudfunctions/queue/index.js
@@ -1,9 +1,9 @@
-// 云函数入口文件
+// 云函数入 口文件
 const cloud = require('wx-server-sdk')
 
 cloud.init()
 
-// 云函数入口函数
+// 云 函数 入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
   
